d1a5a6037466833c6cba5fa4f1eef75a2fda02ff
sinyoung0110 <dpqlc7008@naver.com>
2024-05-08 19:43:44 +0900

    헤더 완성

--- src/components/menus/BasicMenu.js
+++ src/components/menus/BasicMenu.js
@@ -1,31 +1,52 @@
-import { Link } from 'react-router-dom';
-import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const BasicMenu = () => {
-  return (  
-  <nav id='navbar' className="flex bg-blue-300">
-
-    <div className="w-4/5 bg-gray-500" >
-      <ul className="flex p-4 text-white font-bold">
-        <li className="pr-6 text-2xl">
-          <Link to={'/'}>Main</Link>
-        </li>
-        <li className="pr-6 text-2xl">
-          <Link to={'/about'}>About</Link>
-        </li>
-        <li className="pr-6 text-2xl">
-          <Link to={'/todo/'}>Todo</Link>
-        </li>
-      </ul>
-    </div>
-
-    <div className="w-1/5 flex justify-end bg-orange-300 p-4 font-medium">
-        <div className="text-white text-sm m-1 rounded" >
-          Login
-        </div>
-    </div>
-  </nav>
+function BasicMenu() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">IntelliMentor</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+          <NavDropdown title="단어장" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">단어장 생성</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                단어장 목록
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">퀴즈 목록</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="스터디" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">나의 스터디</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                게시판 목록
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">스터디 모집</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="게시판" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">게시판 생성</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                게시판 목록
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">로그아웃</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              마이페이지
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
- 
+
 export default BasicMenu;
--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import BasicMenu from "../components/menus/BasicMenu";
+import { Container } from 'react-bootstrap';
 
 function BasicLayout({children}) {
     return (
         <>
             <BasicMenu/>
-  
+            <Container>
         <div className="bg-white my-5 w-full flex flex-col space-y-1 md:flex-row md:space-x-1 md:space-y-0">
       
       <main 
@@ -15,11 +16,13 @@ function BasicLayout({children}) {
       
       <aside className="bg-green-300 md:w-1/5 lg:w-1/4 px-5 flex py-5"> {/* 상단 여백 py-40 제거 flex 제거 */}
         
-        <h1 className="text-2xl md:text-4xl">Sidebar</h1>
+        <h1 className="text-2xl md:text-4xl">따까리</h1>
 
       </aside>
 
     </div>
+    </Container>
+  
     </>
     );
 }
